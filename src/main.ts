a775e8aca772ef94bf4d38a0c07e5a99f0e286f7
sunnee <sunnee_max@163.com>
2022-12-23 14:46:16 +0800

    electron vue3 vite typescript element-plus

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import "./style.css"
+import App from './App.vue'
+import './samples/node-api'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(ElementPlus)
+app.mount('#app')
+    .$nextTick(() => {
+        postMessage({ payload: 'removeLoading' }, '*')
+    })
